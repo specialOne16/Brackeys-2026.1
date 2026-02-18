77ef62c805d473bf049662429092c0ff1e985465
Yann Demuyt <demisistired@demissexroom.home>
2026-02-18 15:01:25 +0100

    feat: Added debug camera for my poor VR'less ass

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -7,6 +7,13 @@ class_name Player
 @onready var left_xr_controller_3d: XRController3D = $XROrigin3D/LeftXRController3D
 
 func _process(_delta: float) -> void:
+	var interface = XRServer.find_interface("OpenXR")
+	
+	if not interface or not interface.is_initialized():
+		var debug_hand_pos = Vector3(0, 1.5, -0.5) 
+		right_xr_controller_3d.global_position = debug_hand_pos + Vector3(0.2, 0, 0)
+		left_xr_controller_3d.global_position = debug_hand_pos + Vector3(-0.2, 0, 0)
+
 	halberd.look_at_from_position(
 		(right_xr_controller_3d.global_position + left_xr_controller_3d.global_position) / 2,
 		right_xr_controller_3d.global_position
